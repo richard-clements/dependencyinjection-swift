64cc945c62917a5e98658a0b53910f234aa5c18d
Rich <richissomewhere@hotmail.com>
2021-03-02 17:30:30 +0000

    Added LazyInject

--- Sources/DependencyInjection/Inject.swift
+++ Sources/DependencyInjection/Inject.swift
@@ -26,3 +26,43 @@ public struct Inject<T> {
         }
     }
 }
+
+@propertyWrapper
+public class LazyInject<T> {
+    var _wrappedValue: T?
+    
+    private var name: Scope.Name?
+    private var graphResolver: () -> Graph
+    private var arguments: [CVarArg]?
+    
+    public init(name: Scope.Name? = nil, graph: @escaping @autoclosure () -> Graph = .default, arguments: CVarArg...) {
+        self.name = name
+        self.graphResolver = graph
+        self.arguments = arguments
+    }
+    
+    public init(name: Scope.Name? = nil, graph: @escaping @autoclosure () -> Graph = .default) {
+        self.name = name
+        self.graphResolver = graph
+        self.arguments = nil
+    }
+    
+    func resolveValue() -> T {
+        if let name = name, let arguments = arguments {
+            return graphResolver().argumentsResolver(scope: name, with: arguments)!
+        } else if let arguments = arguments {
+            return graphResolver().argumentsResolver(with: arguments)!
+        } else if let name = name {
+            return graphResolver().resolve(scope: name)!
+        } else {
+            return graphResolver().resolve()!
+        }
+    }
+    
+    public var wrappedValue: T {
+        if _wrappedValue == nil {
+            _wrappedValue = resolveValue()
+        }
+        return _wrappedValue!
+    }
+}
--- Tests/DependencyInjectionTests/InjectTests.swift
+++ Tests/DependencyInjectionTests/InjectTests.swift
@@ -64,6 +64,65 @@ public class InjectTests: XCTestCase {
         XCTAssertEqual(mock.integer, 5)
     }
     
+    func testLazyInject() {
+        struct Mock {
+            @LazyInject var integer: Int
+        }
+        var calledDependency = false
+        let graph = Graph {
+            Dependency { (_) -> Int in
+                calledDependency = true
+                return 5
+            }
+        }
+        Graph.default = graph
+        let mock = Mock()
+        XCTAssertFalse(calledDependency)
+        XCTAssertEqual(mock.integer, 5)
+        XCTAssertTrue(calledDependency)
+    }
+    
+    func testLazyInjectNamedScope() {
+        struct Mock {
+            @LazyInject(name: "1") var integer1: Int
+            @LazyInject(name: "2") var integer2: Int
+        }
+        let graph = Graph {
+            Dependency { 1 }
+                .withScope(.named("1"))
+            Dependency { 2 }
+                .withScope(.named("2"))
+        }
+        Graph.default = graph
+        let mock = Mock()
+        XCTAssertEqual(mock.integer1, 1)
+        XCTAssertEqual(mock.integer2, 2)
+    }
+    
+    func testLazyInjectArguments() {
+        struct Mock {
+            @LazyInject(arguments: 1) var integer: Int
+        }
+        let graph = Graph {
+            Dependency { _, arguments in
+                arguments[0] as! Int
+            }
+        }
+        Graph.default = graph
+        let mock = Mock()
+        XCTAssertEqual(mock.integer, 1)
+    }
+    
+    func testLazyInjectGraph() {
+        struct Mock {
+            static let graph = Graph {
+                Dependency { 5 }
+            }
+            @LazyInject(graph: Self.graph) var integer: Int
+        }
+        let mock = Mock()
+        XCTAssertEqual(mock.integer, 5)
+    }
 }
 
 extension InjectTests {
@@ -72,7 +131,11 @@ extension InjectTests {
         ("testInject", testInject),
         ("testInjectNamedScope", testInjectNamedScope),
         ("testInjectArguments", testInjectArguments),
-        ("testInjectGraph", testInjectGraph)
+        ("testInjectGraph", testInjectGraph),
+        ("testLazyInject", testLazyInject),
+        ("testLazyInjectNamedScope", testLazyInjectNamedScope),
+        ("testLazyInjectArguments", testLazyInjectArguments),
+        ("testLazyInjectGraph", testLazyInjectGraph)
     ]
     
 }
